ab87b2ae7ff9d09fe2ee08c4c6f6379bbc852919
Seguineau De Preval Martin <martin.seguineau-de-preval@student-cs.fr>
2025-06-03 15:46:35 +0200

    save models

--- sample_code_submission/neural_network.py
+++ sample_code_submission/neural_network.py
@@ -13,7 +13,7 @@ class NeuralNetwork:
 
     """
 
-    def __init__(self, train_data):
+    def __init__(self, train_data, model_path=None):
         self.model = Sequential()
 
         n_dim = train_data.shape[1]
@@ -25,8 +25,25 @@ class NeuralNetwork:
         self.model.compile(
             loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
+        
+        if model_path is None:
+            self.model_path = os.path.join(os.path.dirname(__file__), "model.h5")
+        else:
+            self.model_path = model_path
         self.scaler = StandardScaler()
 
+
+    def save_model(self,model_path):
+        self.model.save(path)
+        print("Model saved to {path}")
+        joblib.dump(self.scaler, "scaler.pkl")
+    
+    def load_model(self, model_path):
+        self.model= load_model(path)
+        print("Model loaded from {path}")
+        self.scaler = joblib.load("scaler.pkl")
+        
+        
     def fit(self, train_data, y_train, weights_train=None):
 
         self.scaler.fit_transform(train_data)
@@ -34,17 +51,9 @@ class NeuralNetwork:
         self.model.fit(
             X_train, y_train, sample_weight=weights_train, epochs=5, verbose=2
         )
+        self.model.save_model() 
 
     def predict(self, test_data):
         test_data = self.scaler.transform(test_data)
         return self.model.predict(test_data).flatten().ravel()
     
-    def save_model(self, path='model.h5'):
-        self.model.save(path)
-        print("Model saved to {path}")
-        joblib.dump(self.scaler, "scaler.pkl")
-    
-    def load_model(self, path='model.h5'):
-        self.model= load_model(path)
-        print("Model loaded from {path}")
-        self.scaler = joblib.load("scaler.pkl")
