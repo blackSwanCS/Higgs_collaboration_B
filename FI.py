d0137210f25111abca649362adb3da903cad1925
Paul Ménard <paul.menard@student-cs.fr>
2025-06-03 17:13:36 +0200

    black reformatting

--- FI.py
+++ FI.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-file_path = 'blackSwan_data/blackSwan_data.parquet'
+file_path = "blackSwan_data/blackSwan_data.parquet"
 data = pd.read_parquet(file_path)
 
 
@@ -15,11 +15,11 @@ model = joblib.load("models/model.pkl")
 import xgboost as xgb
 
 # Si model est un Booster XGBoost ou un wrapper sklearn
-model.get_booster().get_score(importance_type='weight')
+model.get_booster().get_score(importance_type="weight")
 
 n_cols = data.shape[1]
 n_feat = len(model.feature_importances_)
- 
+
 print(data.columns[:n_feat])
 print(model.feature_importances_)
 
@@ -30,7 +30,7 @@ plt.figure(figsize=(14, 8))
 plt.bar(columns_to_plot, model.feature_importances_)
 plt.xticks(rotation=90)
 plt.title("Feature importances XGBoost Hist")
-#plt.savefig(new_dir + "/VarImp_BDT_XGBoost_Hist.pdf",bbox_inches='tight')
+# plt.savefig(new_dir + "/VarImp_BDT_XGBoost_Hist.pdf",bbox_inches='tight')
 plt.show()
 
-#plt.savefig(new_dir + "/VarImp_BDT_LightGBM.pdf",bbox_inches='tigh
+# plt.savefig(new_dir + "/VarImp_BDT_LightGBM.pdf",bbox_inches='tigh
