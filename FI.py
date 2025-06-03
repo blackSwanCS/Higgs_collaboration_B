c093de7d961029dcc5a4ce292b2e0e6abbe98208
Roy Raphael <raphael.roy@student-cs.fr>
2025-06-03 16:21:12 +0200

    Histogramme des FI

--- FI.py
+++ FI.py
@@ -2,6 +2,7 @@ import pickle
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 file_path = 'blackSwan_data/blackSwan_data.parquet'
 data = pd.read_parquet(file_path)
 
@@ -9,21 +10,27 @@ data = pd.read_parquet(file_path)
 # Charger le modèle
 import joblib
 
-model = joblib.load("scalers/scaler.pkl")
+model = joblib.load("models/model.pkl")
 
 import xgboost as xgb
 
 # Si model est un Booster XGBoost ou un wrapper sklearn
 model.get_booster().get_score(importance_type='weight')
 
+n_cols = data.shape[1]
+n_feat = len(model.feature_importances_)
+ 
+print(data.columns[:n_feat])
+print(model.feature_importances_)
 
-plt.bar(data.columns.values, xgb.feature_importances_)
+# 4. Prendre les bonnes colonnes si nécessaire
+columns_to_plot = data.columns[:n_feat]
+
+plt.figure(figsize=(14, 8))
+plt.bar(columns_to_plot, model.feature_importances_)
 plt.xticks(rotation=90)
 plt.title("Feature importances XGBoost Hist")
 #plt.savefig(new_dir + "/VarImp_BDT_XGBoost_Hist.pdf",bbox_inches='tight')
 plt.show()
-plt.bar(data.columns.values, gbm.feature_importances_)
-plt.xticks(rotation=90)
-plt.title("Feature importances LightGBM")
-#plt.savefig(new_dir + "/VarImp_BDT_LightGBM.pdf",bbox_inches='tight')
-plt.show()
+
+#plt.savefig(new_dir + "/VarImp_BDT_LightGBM.pdf",bbox_inches='tigh
