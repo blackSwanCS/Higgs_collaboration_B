27f5fb0fe0c162c2afeb2ca23d332e7a86640d35
Paul Ménard <paul.menard@student-cs.fr>
2025-06-03 16:37:55 +0200

    Revert "reformatted everything in order to merge"
    This reverts commit 9584cc83e6b3e9518b453d2f1e8be0d33aeb396b.

--- sample_code_submission/systematic_analysis_V2.py
+++ sample_code_submission/systematic_analysis_V2.py
@@ -4,7 +4,9 @@ from model import Model
 from HiggsML.datasets import download_dataset
 from utils import histogram_dataset
 
-data = download_dataset("blackSwan_data")
+data = download_dataset(
+    "blackSwan_data"
+) 
 
 data.load_train_set()
 data_set = data.get_train_set()
@@ -29,48 +31,44 @@ def tes_fitter(
     """
     signal_field = train_set[target == 1]
     background_field = train_set[target == 0]
-
+    
     syst_set_signal = systematics(signal_field, tes=1)
     score_signal = model.predict(syst_set_signal["data"])
-    histogram_nominal_signal, _ = np.histogram(
-        score_signal, bins=100, range=(0, 1), weights=train_set["weights"]
-    )
+    histogram_nominal_signal, _ = np.histogram(score_signal, bins=100, range=(0, 1), weights = train_set["weights"])
 
     syst_set_background = systematics(background_field, tes=1)
     score_background = model.predict(syst_set_background["data"])
-    histogram_nominal_background, _ = np.histogram(
-        score_background, bins=100, range=(0, 1), weights=train_set["weights"]
-    )
+    histogram_nominal_background, _ = np.histogram(score_background, bins=100, range=(0, 1), weights = train_set["weights"])
+
 
     first_bin_nominal_signal = histogram_nominal_signal[0]
     first_bin_nominal_background = histogram_nominal_background[0]
 
+
     delta_S_signal = []
     delta_S_background = []
-
+    
     tes_range = np.linspace(0.9, 1.1, 5)
     for tes in tes_range:
-
+        
         syst_set_signal = systematics(signal_field, tes)
         score_signal = model.predict(syst_set_signal["data"])
-        histogram_signal, _ = np.histogram(
-            score_signal, bins=100, range=(0, 1), weights=train_set["weights"]
-        )
-
+        histogram_signal, _ = np.histogram(score_signal, bins=100, range=(0, 1), weights = train_set["weights"])
+        
         syst_set_signal = systematics(background_field, tes)
         score_signal = model.predict(syst_set_background["data"])
-        histogram_background, _ = np.histogram(
-            score_signal, bins=100, range=(0, 1), weights=train_set["weights"]
-        )
-
+        histogram_background, _ = np.histogram(score_signal, bins=100, range=(0, 1), weights = train_set["weights"])
+        
         first_bin_signal = histogram_signal[0]
         first_bin_background = histogram_background[0]
-
+        
         delta_signal = first_bin_signal - first_bin_nominal_signal
         delta_background = first_bin_background - first_bin_nominal_background
-
+        
         delta_S_signal.append(delta_signal)
         delta_S_background.append(delta_background)
+        
+
 
     # Write a function to loop over different values of tes and histogram and make fit function which transforms the histogram for any given TES
 
