7f5cc94b1ed2984433d4a1b043f22072f937c474
Benjamin Plumejeau <benjamin.plumejeau@student-cs.fr>
2025-06-04 16:54:21 +0200

    calcul of the shifted values of the score for the estimated tes

--- sample_code_submission/systematic_analysis_V2.py
+++ sample_code_submission/systematic_analysis_V2.py
@@ -37,23 +37,23 @@ def tes_fitter(model, train_set, nbin=25):
     score_background = model.predict(background_field)
     background_weights = nominal_syst_set["weights"][target == 0]
     histogram_nominal_background, _ = np.histogram(score_background, bins=nbin, range=(0, 1), weights=background_weights)
-    
+
 
     delta_S_signal = []
     delta_S_background = []
-    
+
     tes_range = np.linspace(0.9, 1.1, 10)
-    
+
     for i in range(nbin):
         bin_delta_S_signal = []
-        bin_delta_S_background = []  
+        bin_delta_S_background = []
         for tes in tes_range:
-            
+
             syst_set = systematics(train_set, tes)
             target = syst_set["labels"]
             signal_field = syst_set["data"][target == 1]
             background_field = syst_set["data"][target == 0]
-            
+
             # Signal
             score_signal = model.predict(signal_field)
             weights_signal = syst_set["weights"][target == 1]
@@ -77,12 +77,68 @@ def tes_fitter(model, train_set, nbin=25):
         delta_S_background.append(bin_delta_S_background)
     # Write a function to loop over different values of tes and histogram and make fit function which transforms the histogram for any given TES
 
-    def fit_function(array, tes):
-        # Dummy fit function, replace with actual fitting procedure
-        return array * tes
+    def fit_function(array, maxi=2):
+        """tes est toujours défini entre 0.9 et 1.1
+        array correspond à la liste des valeurs prise par l'histogramme après isolement d'un bin
+        """
+        meilleur = 1
+        R_meilleur = np.inf
+        R = 0
+        for deg in range(0, maxi + 1):
+            parameters = np.polyfit(tes_range, array, deg)
+            # print(f"{deg} : {parameters}")
+            for ind in range(len(tes_range)):
+                y = 0
+                for i in range(deg + 1):
+                    y += parameters[i] * tes_range[ind] ** (deg - i)
+                R += (y - array[ind]) ** 2
+            if R < R_meilleur:
+                meilleur = deg
+                R_meilleur = R
+            R = 0
+        # print("meilleur :", meilleur)
 
-    return fit_function
+        return np.polyfit(tes_range, array, meilleur)
 
+
+
+    return (fit_function, delta_S_signal, delta_S_background)
+
+def give_fitting_functions  (fitfunction, delta_S_signal, delta_S_background, maxi=2):
+    '''returns the coeeficients for the polynomial fitting function for all bins'''
+    signal_fitting_pol = [fitfunction(delta_S_signal[i], maxi=maxi) for i in range(len(delta_S_signal))]
+    background_fitting_pol = [fitfunction(delta_S_background[i], maxi=maxi) for i in range(len(delta_S_background))]
+    return(signal_fitting_pol,background_fitting_pol)
+
+
+def shifted_score(alpha, train_set, model, signal_fitting_pol, background_fitting_pol, nbin=25):
+    '''returns the shifted values for the score'''
+    syst_set = systematics(train_set, tes=alpha)
+    target = syst_set["labels"]
+    signal_field = syst_set["data"][target == 1]
+    background_field = syst_set["data"][target == 0]
+    
+    # Signal
+    score_signal = model.predict(signal_field)
+    weights_signal = syst_set["weights"][target == 1]
+    histogram_signal, _ = np.histogram(score_signal, bins=nbin, range=(0, 1), weights=weights_signal)
+
+    # Background
+    score_background = model.predict(background_field)
+    weights_background = syst_set["weights"][target == 0]
+    histogram_background, _ = np.histogram(score_background, bins=nbin, range=(0, 1), weights=weights_background)
+
+    delta_N_signal = np.array([np.polyval(signal_fitting_pol[i][::-1], alpha) for i in range(len(signal_fitting_pol))])
+    delta_N_background = np.array([np.polyval(background_fitting_pol[i][::-1], alpha) for i in range(len(background_fitting_pol))])
+    
+    shifted_histogram_signal = histogram_signal + delta_N_signal
+    shifted_histogram_background = histogram_background + delta_N_background
+
+    return(shifted_histogram_signal, shifted_histogram_background)
+
+def plot_shifted_score_histograms(shifted_histogram_signal, shifted_histogram_background):
+    '''plot the histograms for the shifted score'''
+    return 1  # to do
 
 def jes_fitter(
     model,
